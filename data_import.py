5d1fa2f49feff8e0c62834331a0c56a6d7a3fa53
Cole Watson <Cole@MacBook-Pro-2.local>
2016-01-20 19:03:49 -0800

    Added entropy function

--- data_import.py
+++ data_import.py
@@ -2,8 +2,9 @@
 def data_reader(loc):
 	import csv
 	test_data = []
-	with open('data_sets1/test_set.csv', 'rb') as csvfile:
+	with open(loc, 'rb') as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
-			test_data.append(row)
+			row_int = dict((k,int(v)) for k,v in row.iteritems())
+			test_data.append(row_int)
 	return test_data
