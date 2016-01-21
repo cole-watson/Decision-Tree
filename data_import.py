8987c242a9f1ead5672f15248ed6c80871315d57
FrankieG <Frankie@Uoregon.edu>
2016-01-20 18:29:51 -0800

    Added data reader

--- data_import.py
+++ data_import.py
@@ -2,7 +2,7 @@
 def data_reader(loc):
 	import csv
 	test_data = []
-	with open('data_sets1/test_set.csv', 'rb') as csvfile:
+	with open(loc, 'rb') as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
 			test_data.append(row)
