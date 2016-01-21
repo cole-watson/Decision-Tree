be929f857bb6e2d6e1ca0f3d00d314db9ffd7030
Cole Watson <Cole@MacBook-Pro-2.local>
2016-01-20 19:01:27 -0800

    Added entropy function

---
+++ entropy.py
@@ -0,0 +1,16 @@
+#calculate the entropy of a give data set
+import math
+
+def entropy(data):
+	strong = 0
+	weak = 0
+	for dic in data:
+		if dic['Class'] == 1:
+			strong += 1
+		else:
+			weak += 1
+	total = strong + weak
+	ent = -(strong/total)(math.log((strong/total), 2)) - (weak/total)(math.log((weak/total), 2))
+
+	return ent
+
